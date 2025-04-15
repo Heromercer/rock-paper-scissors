229b1fad11f30aca168c91262ada08a77a8386df
Alec Lawrence <150299829+Heromercer@users.noreply.github.com>
2025-04-15 17:19:33 -0400

    Add functions for getting human and computer choices

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+function getComputerChoice() {
+    const minCeiled = Math.ceil(0);
+    const maxFloored = Math.floor(2);
+    const ranNum =  Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+
+    if (ranNum == 0) {
+        return "rock";
+    } else if (ranNum == 1) {
+        return "paper";
+    } else if (ranNum == 2) {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Pick rock, paper, or scissors");
+    if (userInput.toLowerCase() == "rock") {
+        return "rock";
+    } else if (userInput.toLowerCase() == "paper") {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
